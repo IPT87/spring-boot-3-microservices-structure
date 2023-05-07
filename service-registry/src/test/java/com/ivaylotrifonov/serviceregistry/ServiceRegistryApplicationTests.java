909db383c90fffa876f98d2ab71438fb2c697f80
Ivaylo <itrifonov87@gmail.com>
2023-05-07 14:19:59 +0200

    Service-registry base structure created.

---
+++ service-registry/src/test/java/com/ivaylotrifonov/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivaylotrifonov.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
